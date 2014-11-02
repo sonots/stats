85876ee1eabfdcdf66a3158762d967080357581c
sonots <sonots@gmail.com>
2014-11-02 16:45:29 +0900

    initial

---
+++ README.md
@@ -0,0 +1,98 @@
+# stats
+
+Calculate statistics from lines
+
+# Description
+
+`stats` is a command line tool written in golang to calculate statistics from lines.
+
+Example 1: 
+
+```bash
+$ echo "/me\t0.234\n/me\t1.234" | stats -f 2
+max:1.234   min:0.234   count:2   mean:0.734  sum:1.468
+```
+
+Example 2: 
+
+```bash
+$ echo "/me\t0.234\n/me\t1.234\n/books\t1.111" | stats -k 1 -f 2
+key:/me      count:2   max:1.234   min:0.234   mean:0.734  sum:1.468
+key:/books   count:1   max:1.111   min:1.111   mean:1.111  sum:1.111
+```
+
+Example 3: 
+
+```bash
+$ echo "GET\t/me\t0.234\nPUT\t/me\t1.234\nGET\t/books\t1.111" | stats -k 1,2 -f 3
+key:GET /me      count:1   max:0.234   min:0.234   mean:0.234  sum:0.234
+key:PUT /me      count:1   max:1.234   min:1.234   mean:1.234  sum:1.234
+key:GET /books   count:1   max:1.111   min:1.111   mean:1.111  sum:1.111
+```
+
+## Tips
+
+[lltsv](https://github.com/sonots/lltsv) would be useful to parse LTSV format log:
+
+
+```bash
+$ echo "path:/me\treqtime:0.234\npath:/me\treqtime:1.234" | lltsv -k path,reqtime -K | stats -k 1 -f 2
+max:1.234   min:0.234   count:2   mean:0.734  sum:1.468
+```
+
+## Installation
+
+Executable binaries are available at [releases](https://github.com/sonots/stats/releases).
+
+For example, for linux x86_64, 
+
+```bash
+$ wget https://github.com/sonots/stats/releases/download/v0.1.0/stats_linux_amd64 -O stats
+$ chmod a+x stats
+```
+
+If you have the go runtime installed, you may use go get. 
+
+```bash
+$ go get github.com/sonots/stats
+```
+
+## ToDo
+
+1. write tests
+
+## Build
+
+To build, use go get and make
+
+```
+$ go get -d github.com/sonots/stats
+$ cd $GOPATH/src/github.com/sonots/stats
+$ make
+```
+
+To release binaries, I use [gox](https://github.com/mitchellh/gox) and [ghr](https://github.com/tcnksm/ghr)
+
+```
+go get github.com/mitchellh/gox
+gox -build-toolchain # only first time
+go get github.com/tcnksm/ghr
+
+cd $GOPATH/src/github.com/sonots/stats/pkg
+gox ../...
+ghr <tag> .
+```
+
+## Contribution
+
+1. Fork (https://github.com/sonots/stats/fork)
+2. Create a feature branch
+3. Commit your changes
+4. Rebase your local changes against the master branch
+5. Run test suite with the go test ./... command and confirm that it passes
+6. Run gofmt -s
+7. Create new Pull Request
+
+## Copyright
+
+See [LICENSE](./LICENSE)
---
+++ stats.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strings"
+
+	"github.com/andrew-d/go-termutil"
+	"github.com/codegangsta/cli"
+	"github.com/mgutz/ansi"
+)
+
+// os.Exit forcely kills process, so let me share this global variable to terminate at the last
+var exitCode = 0
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "stats"
+	app.Version = Version
+	app.Usage = `Calculate statistics from lines
+
+	Example1 $ echo "/me\t0.234\n/me\t1.234" | stats -f 2
+	max:1.234   min:0.234   count:2   mean:0.734
+
+	The output is colorized as default when you outputs to a terminal. 
+	The coloring is disabled if you pipe or redirect outputs.
+
+	Example2 $ echo "foo:aaa\tbar:bbb" | lltsv -k foo,bar -K
+	aaa       bbb
+
+	You may eliminate labels with "-K" option`
+	app.Author = "sonots"
+	app.Email = "sonots@gmail.com"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "key, k",
+			Usage: "keys to output (multiple keys separated by ,)",
+		},
+		cli.BoolFlag{
+			Name:  "no-key, K",
+			Usage: "output without keys (and without color)",
+		},
+	}
+	app.Action = doMain
+	app.Run(os.Args)
+	os.Exit(exitCode)
+}
+
+func doMain(c *cli.Context) {
+	keys := make([]string, 0, 0) // slice with length 0
+	if c.String("key") != "" {
+		keys = strings.Split(c.String("key"), ",")
+	}
+	no_key := c.Bool("no-key")
+	funcAppend := getFuncAppend(no_key)
+
+	var file *os.File
+	if len(c.Args()) > 0 {
+		filename := c.Args()[0]
+		var err error
+		file, err = os.Open(filename)
+		if err != nil {
+			os.Stderr.WriteString("failed to open and read " + filename)
+			exitCode = 1
+			return
+		}
+	} else {
+		file = os.Stdin
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lvs := parseLtsv(line)
+		ltsv := restructLtsv(keys, lvs, funcAppend)
+		os.Stdout.WriteString(ltsv + "\n")
+	}
+	if err := scanner.Err(); err != nil {
+		os.Stderr.WriteString("reading standard input errored")
+		exitCode = 1
+		return
+	}
+}
+
+func restructLtsv(keys []string, lvs map[string]string, funcAppend func([]string, string, string) []string) string {
+	// specified keys or all keys
+	orders := keys
+	if len(keys) == 0 {
+		orders = keysInMap(lvs)
+	}
+	// make slice with enough capacity so that append does not newly create object
+	// cf. http://golang.org/pkg/builtin/#append
+	selected := make([]string, 0, len(orders))
+	for _, label := range orders {
+		value := lvs[label]
+		selected = funcAppend(selected, label, value)
+	}
+	return strings.Join(selected, "\t")
+}
+
+func parseLtsv(line string) map[string]string {
+	columns := strings.Split(line, "\t")
+	lvs := make(map[string]string)
+	for _, column := range columns {
+		l_v := strings.SplitN(column, ":", 2)
+		if len(l_v) < 2 {
+			continue
+		}
+		label, value := l_v[0], l_v[1]
+		lvs[label] = value
+	}
+	return lvs
+}
+
+// Return function pointer to avoid `if` evaluation occurs in each iteration
+func getFuncAppend(no_key bool) func([]string, string, string) []string {
+	if no_key {
+		return func(selected []string, label string, value string) []string {
+			return append(selected, value)
+		}
+	} else {
+		if termutil.Isatty(os.Stdout.Fd()) {
+			return func(selected []string, label string, value string) []string {
+				return append(selected, ansi.Color(label, "green")+":"+ansi.Color(value, "magenta"))
+			}
+		} else {
+			// if pipe or redirect
+			return func(selected []string, label string, value string) []string {
+				return append(selected, label+":"+value)
+			}
+		}
+	}
+}
+
+func keysInMap(m map[string]string) []string {
+	keys := make([]string, 0, len(m))
+	for key := range m {
+		keys = append(keys, key)
+	}
+	return keys
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
